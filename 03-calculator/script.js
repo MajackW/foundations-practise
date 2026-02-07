0211f73676fd414b4a786fe28f8c581952f6d343
majack <majackwariaka@gmail.com>
2026-02-07 23:27:02 +0300

    added logic to split the expression into * num1 num2 and operator added logic to change string inputs to numbers added function to evaluate expression and send the answer to display

--- 03-calculator/script.js
+++ 03-calculator/script.js
@@ -28,3 +28,42 @@ function display(value){
 function checkValues(container){
     const content = container.textContent;
 }
+function evaluate(exp){
+    let operands = ["-", "+"];
+    let operators = ["-","+","/","x","^"];
+    let num1 = "+";
+    let num2 = "";
+    let operand = "";
+    let i = 0;
+    while(i < exp.length){
+        conditionNeg = (exp.at(i) === "-" && num1 === "+" ||
+                        exp.at(i) === "+" && num1 === "-")
+        conditionPos = (exp.at(i) === "-" && num1 === "-")
+        if(operators.includes(exp.at(i)) && num1 === ""){
+            num1 += exp.at(i);
+        }else if (conditionPos){
+            num1 = "+";
+        }else if(conditionNeg){
+            num1 = "-"
+        }else if(!(operators.includes(exp.at(i))) && (operators.includes(num1) || isNumeric(exp.at(i-1)))){
+            num1 += exp.at(i);
+        }else if(operators.includes(exp.at(i)) && isNumeric(num1.at(-1))){
+            operand = exp.at(i);
+        }else if(operand != "" && num2 === ""){
+            num2 += exp.at(i);
+        }
+        i++;
+    }
+    num1 = Number(num1);
+    num2 = Number(num2);
+    display("clear");
+    display(num1+num2);
+    console.log(num1);
+    console.log(operand);
+    console.log(num2);
+
+}
+function isNumeric(val){
+    const num = Number(val);
+    return !Number.isNaN(num);
+}
